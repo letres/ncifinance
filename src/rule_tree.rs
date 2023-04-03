4f8b3c09de52d3f8395b17d85084ce814dfdcc39
letres <letres@github.com>
2023-04-03 23:51:15 +0000

    Continuing work on rule tree, adding traits for ease of use.

--- src/rule_tree.rs
+++ src/rule_tree.rs
@@ -1,44 +1,74 @@
-use ta::DataItem
 use ta::{
-    indicators::{FastStochastic, RelativeStrengthIndex},
+    indicators::{
+        //Oscilattors
+        FastStochastic, RelativeStrengthIndex, 
+        MovingAverageConvergenceDivergence,        
+    },
     Next,
+    DataItem,
 };
 
+//To add novel features, impl Update with a custom Type for the struct
 
-pub trait NodeCalc {
-    fn calc(&self, DataItem) -> f64;
-}
-
-enum Indicator {
-    MovingAverage,
-    RSI,
-    MACD,
-    // add more indicators as needed
+pub trait NodeCalc<T> {
+    fn compute_tree(&self, DataItem) -> f64;
+    fn update_calc(&self,T);
 }
 
 
 
 enum Comparison {
     GreaterThan,
-    LessThan,
-    EqualTo,
-    // add more comparisons as needed
+    LessThanEq,
 }
 
-enum BooleanOp {
-    And,
-    Or,
-    // add more boolean operators as needed
+struct ComparisonNode {
+    pub left: Box<NodeCalc>,
+    pub right: Box<NodeCalc>,
+    pub comparison: Comparison,
 }
 
+
+impl NodeCalc for ComparisonNode{
+    fn compute_tree(&self, data:DataItem)-> f64 {
+        match self.comparison {
+            Comparison::GreaterThan => (left.compute_tree(data) > right.calc(data)) as f64;
+            Comparison::LessThanEq => (left.compute_tree(data) <= right.calc(data)) as f64;
+        }
+    }
+}
+
+struct ConstantNode {
+    pub value: f64,
+}
+
+impl NodeCalc for ConstantNode{
+    fn compute_tree(&self, _data:DataItem)-> f64 {
+        self.value
+    }
+    fn update_calc(&self,_:T){}
+}   
+
+
+//This should just be a wrapper (or less) around ta indicators
+enum Indicator {
+    SMA,
+    EMA,
+    RSI,
+    MACD,
+    // add more indicators as needed
+}
+
+//This needs to take in the value from the previous total computation
 struct Lag {
     pub value: f64,
 }
 
-struct Constant {
-    pub value: f64,
-}
 
+//Max and Min is equivilent to (N)Or
+
+
+//Multiply is equivilant to to an AND on Binary inputs
 struct Multiply {
     pub left: Box<Node>,
     pub right: Box<Node>,
@@ -48,23 +78,12 @@ struct Average {
     pub nodes: Vec<Box<Node>>,
 }
 
-struct ComparisonNode {
-    pub left: Box<Node>,
-    pub right: Box<Node>,
-    pub comparison: Comparison,
-}
 
-struct BooleanOpNode {
-    pub left: Box<Node>,
-    pub right: Box<Node>,
-    pub op: BooleanOp,
-}
-
+//The Nodes will Eval by passing a DataItem down the Tree
 enum Node {
     IndicatorNode { indicator: Indicator, lag: Lag },
-    ConstantNode(Constant),
+    ConstantNode(ConstantNode),
     MultiplyNode(Multiply),
     AverageNode(Average),
     ComparisonNode(ComparisonNode),
-    BooleanOpNode(BooleanOpNode),
 }
