4fdc2f87f6b866e532a8ab3abffe09ea4683b31b
letres <letres@github.com>
2023-04-04 04:59:18 +0000

    Just need to assemble a basic version then it should be good to test the new rule system.

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
   inputs.nci.url = "github:yusdacra/nix-cargo-integration";
   inputs.nci.inputs.nixpkgs.follows = "nixpkgs";
-  #inputs.parts.url = "github:hercules-ci/flake-parts";
+  inputs.parts.url = "github:hercules-ci/flake-parts";
   inputs.parts.inputs.nixpkgs-lib.follows = "nixpkgs";
   inputs.devshell.url = "github:numtide/devshell";
 
--- src/rule_tree.rs
+++ src/rule_tree.rs
@@ -12,11 +12,11 @@ use ta::{
 
 pub trait NodeCalc<T> {
     fn compute_tree(&self, DataItem) -> f64;
-    fn update_calc(&self,T);
+    fn update_calc(&mut self,T);
 }
 
 
-
+//Comparison Node
 enum Comparison {
     GreaterThan,
     LessThanEq,
@@ -28,15 +28,19 @@ struct ComparisonNode {
     pub comparison: Comparison,
 }
 
-
 impl NodeCalc for ComparisonNode{
     fn compute_tree(&self, data:DataItem)-> f64 {
         match self.comparison {
-            Comparison::GreaterThan => (left.compute_tree(data) > right.calc(data)) as f64;
-            Comparison::LessThanEq => (left.compute_tree(data) <= right.calc(data)) as f64;
+            Comparison::GreaterThan => (left.compute_tree(data) > right.compute_tree(data)) as f64;
+            Comparison::LessThanEq => (left.compute_tree(data) <= right.compute_tree(data)) as f64;
         }
     }
+    fn update_calc(&mut self, data:T){
+        left.compute_tree(data);
+        right.compute_tree(data);
+    }
 }
+
 
 struct ConstantNode {
     pub value: f64,
@@ -50,40 +54,47 @@ impl NodeCalc for ConstantNode{
 }   
 
 
-//This should just be a wrapper (or less) around ta indicators
-enum Indicator {
-    SMA,
-    EMA,
-    RSI,
-    MACD,
-    // add more indicators as needed
-}
-
-//This needs to take in the value from the previous total computation
+//This needs to take in the value from the previous total computationThis should work.
 struct Lag {
     pub value: f64,
 }
 
+struct LagNode{
+    pub value: f64,
+}
 
-//Max and Min is equivilent to (N)Or
+impl NodeCalc for LagNode{
+    fn compute_tree(&self, _data:DataItem)-> f64 {
+        self.value
+    }
+    fn update_calc(&mut self,data:Lag){
+        self.value = data.value;
+    }
+}   
 
 
-//Multiply is equivilant to to an AND on Binary inputs
+//This should just be a wrapper (or less) around ta indicators
+//Should be done first, This combined with Constant and Compare should 
+//Be enough to imitate the example engine5
+struct IndicatorNode<T:ta::Next<f64>>(T)
+
+impl NodeCalc for IndicatorNode{
+    fn compute_tree(&self, data:DataItem)-> f64 {
+        self.next(data)
+    }
+    fn update_calc(&mut self,_:T){}
+}   
+//TODO:Max and Min is equivilent to (N)Or
+
+
+//TODO:Multiply is equivilant to to an AND on Binary inputs 
 struct Multiply {
     pub left: Box<Node>,
     pub right: Box<Node>,
 }
 
+//TODO: This will do some nice things
 struct Average {
     pub nodes: Vec<Box<Node>>,
 }
 
-
-//The Nodes will Eval by passing a DataItem down the Tree
-enum Node {
-    IndicatorNode { indicator: Indicator, lag: Lag },
-    ConstantNode(ConstantNode),
-    MultiplyNode(Multiply),
-    AverageNode(Average),
-    ComparisonNode(ComparisonNode),
-}
